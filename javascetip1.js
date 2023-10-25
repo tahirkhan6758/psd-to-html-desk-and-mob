c0a4326bde14b0e727d96459692c5b4cca41ab8e
tahirkhan6758 <148421003+tahirkhan6758@users.noreply.github.com>
2023-10-25 03:12:05 -0600

    Add files via upload

---
+++ javascetip1.js
@@ -0,0 +1,15 @@
+let navmenu=document.getElementById("fabars")
+
+
+function shownav(){
+ 
+ navmenu.style.transform='translateX(0px)';
+ 
+ 
+}
+
+function hidenav(){
+    
+    navmenu.style.transform='translateX(150px)';
+   
+}
